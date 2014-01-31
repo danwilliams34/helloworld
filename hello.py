123d1a94c2a4a88f3e2e22e67cb089edb466a100
danwilliams34 <daniel.williams1@ntlworld.com>
2014-01-31 14:51:27 +0000

    Update hello.py
    With a function to determine if a number is a factor or not

--- hello.py
+++ hello.py
@@ -2,3 +2,10 @@ def hello():
     print("Hello World")
     
 hello()
+
+def isfactor (a,b):
+    return (a%b==0)
+print(isfactor(3,6))
+#output should be True
+print(isfactor(7,9))
+#output should be False
